45894d06d7f776ae71a383816b05c78aef807643
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-08-01 16:15:29 +0530

    Data is connected

--- client/script.js
+++ client/script.js
@@ -74,6 +74,36 @@ const handleSubmit = async(e)=>{
   const messageDiv = document.getElementById(uniqueId);
 
   loader(messageDiv);
+
+  //fetch data from server --> bot's response
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok){
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  }
+  else{
+    const err = await response.text();
+
+    messageDiv.innerHTML = 'Something went wrong';
+    
+    alert(err);
+    
+  }
 };
 
 form.addEventListener('submit', handleSubmit);
